a9e0621db567aa5417877ff5cbdd49fc93554baf
Vasu Sharma <vsh.9705@gmail.com>
2025-06-18 23:37:16 +0000

    First commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+function NotFound() {
+    return (
+        <>
+        <div>
+            <h1>404 Not Found</h1>
+            <p>The Page you're looking for does not exist!</p>
+        </div>
+        </>
+    )
+}
+
+export default NotFound
